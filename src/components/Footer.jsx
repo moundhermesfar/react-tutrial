36f9649dd30f8a091c2fe3b225f12e8adab7c44d
moundher mesfar <mesfarmounether@gmail.com>
2024-03-28 23:15:38 +0100

    adding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
+import { FaFacebook, FaInstagram, FaEnvelope } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer className="footer">
+      <div className="social-icons">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://instgram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://gmail.com">
+          <FaEnvelope />
+        </a>
+      </div>
+      <p>2020 CARSSO, All rights reseved.</p>
+    </footer>
+  );
 }
