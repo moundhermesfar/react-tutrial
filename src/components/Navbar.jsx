d48e66efdbc4fb680c61ff1497aa0b2faa11f384
moundher mesfar <mesfarmounether@gmail.com>
2024-03-28 23:07:12 +0100

    adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
-
 function Navbar() {
+
+    const links = [
+        {name: 'HOME'},
+        {name: 'ABOUT'},
+        {name: 'CARS'},
+        {name: 'CONTACT'},
+    ]
+
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="navbar">
+      <div>
+        <h1 className="logo">CASRSSO</h1>
+      </div>
+      <div className="links">
+        <ul>
+         {links.map((link, index) => (
+            <li className="link" key={index}>{link.name}</li>
+         ))}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
