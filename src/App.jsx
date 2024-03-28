0d596f8c62d45e67fb130e3ba4e36f4a860b583d
moundher mesfar <mesfarmounether@gmail.com>
2024-03-28 22:54:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  ) 
+}
+
+export default App
